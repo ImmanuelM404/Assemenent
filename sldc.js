d137c575fce38155f6374f01deda3c8c9bad4fc4
ImmanuelM404 <immanuelmamou@gamil.com>
2021-10-15 14:33:35 -0700

    Initial Commit

---
+++ sldc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+let SDL = ['Planning', 'Analysis of Requirements','Design', 'Implementation', 'Testing & Integration', 'Maintain']
+for (let i = 0; i < SDL.length; i++){
+    console.log(SDL[i])
+}
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+let Planning = ''
+let AnalysisOfRequirements = ''
+let Design = ''
+let Implementation = ''
+let TestingIntegration = ''
+let Maintain = ''
+console.log(Planning)
